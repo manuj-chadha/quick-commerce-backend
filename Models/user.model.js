8c03de729d9ae8c3c675f3f86e96a9e9c51cc392
Akshat090803 <akshatjain70233@gmail.com>
2025-07-28 16:08:31 +0530

    Fix: Corrected timestamps option in user model

--- Models/user.model.js
+++ Models/user.model.js
@@ -46,7 +46,7 @@ const userSchema = new mongoose.Schema({
       ref:'Product'
     }]
 }, {
-    timestamps: TextTrackCue
+    timestamps: true 
 });
 
 module.exports = mongoose.model('User', userSchema);

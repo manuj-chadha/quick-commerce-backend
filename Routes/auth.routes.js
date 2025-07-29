002a5474ff9c1f9cd3f479cd9943173a89bb6822
Manuj Chadha <manujchadha7777@gmail.com>
2025-07-29 14:01:41 +0530

    Update auth.routes.js

--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const { signup, signin } = require('../Controllers/user.controller');
+const { signup, signin, getAllUsers } = require('../Controllers/user.controller');
+const { protect, admin } = require('../Middlewares/auth.middleware');
+
 const router = express.Router();
 
+// Public Routes
 router.post('/signup', signup);
 router.post('/signin', signin);
+
+// Admin-only Route
+router.get('/', protect, admin, getAllUsers);
 
 module.exports = router;

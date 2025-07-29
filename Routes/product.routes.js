014033e3f559dd6778f035345213e40655b13173
Manuj Chadha <manujchadha7777@gmail.com>
2025-07-29 17:47:09 +0530

    Update product.routes.js

--- Routes/product.routes.js
+++ Routes/product.routes.js
@@ -1,10 +1,26 @@
 const express = require('express');
-const { addProduct, getProducts, getProductById, updateProduct, deleteProduct } = require('../Controllers/product.controller');
+const {
+  addProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} = require('../Controllers/product.controller');
 const { protect, admin } = require('../Middlewares/auth.middleware');
 const upload = require('../Utilities/cloudinary');
 const router = express.Router();
 
-router.route('/').get(getProducts).post(protect, admin, upload.array('images', 5), addProduct);
-router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+// GET all, POST new product
+router
+  .route('/')
+  .get(getProducts)
+  .post(protect, admin, upload.array('images', 5), addProduct);
+
+// GET single, UPDATE, DELETE product
+router
+  .route('/:id')
+  .get(getProductById)
+  .put(protect, admin, upload.array('images', 5), updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 module.exports = router;

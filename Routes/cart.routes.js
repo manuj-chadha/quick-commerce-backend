bb615a3071eeda5cdfa57366d7c8013cef1528a9
Manuj Chadha <manujchadha7777@gmail.com>
2025-07-31 22:16:33 +0530

    Update cart.routes.js

--- Routes/cart.routes.js
+++ Routes/cart.routes.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const { getCart, addToCart, removeFromCart } = require('../Controllers/cart.controller');
+const {
+  getCart,
+  addToCart,
+  removeFromCart,
+  updateCartItem,
+  clearCart
+} = require('../Controllers/cart.controller');
 const { protect } = require('../Middlewares/auth.middleware');
+
 const router = express.Router();
 
-router.route('/').get(protect, getCart).post(protect, addToCart);
-router.route('/:productId').delete(protect, removeFromCart);
+// Get & Add to cart
+router.route('/')
+  .get(protect, getCart)
+  .post(protect, addToCart)
+  .delete(protect, clearCart);
+
+// Update or remove specific item
+router.route('/:productId')
+  .patch(protect, updateCartItem)
+  .delete(protect, removeFromCart);
 
 module.exports = router;
